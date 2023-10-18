dd5b882280bc221c10deb2985f59afef45630460
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-18 16:54:08 -0300

    html c refact

--- assets/libs/html-component.js
+++ assets/libs/html-component.js
@@ -1,78 +1,55 @@
 window.HTMLComponentRender = container => {
-    container.querySelectorAll('html-component').forEach(async HTMLComponent => {
+    container.querySelectorAll('html-c').forEach(async HTMLComponent => {
         if(HTMLComponent.hasAttribute('rendered')) return
 
-        const res = await fetch(HTMLComponent.getAttribute('src'));
-        let text = await res.text();
+        const srcRes = await fetch(HTMLComponent.getAttribute('src'));
+        let srcText = await srcRes.text();
 
         HTMLComponent.setAttribute('children', HTMLComponent.innerHTML);
         HTMLComponent.innerHTML = '';
 
         const propsRegex = /\{(\w+)\}/g;
 
-        if(HTMLComponent.hasAttribute('shadow-dom')) {
-            HTMLComponent.attachShadow({mode: "open"});
+        function loadScripts(scriptElements){
+            scriptElements.forEach(async scriptElement => {
+                const newScriptElement = document.createElement('script')
 
-            HTMLComponent.shadowRoot.innerHTML = text.replace(propsRegex, (match, att) =>
-            HTMLComponent.getAttribute(att) || ''
-            )
-
-            HTMLComponent.shadowRoot.querySelectorAll('script').forEach(async scriptElement => {
-                if (scriptElement.hasAttribute('src')) {
+                if(scriptElement.hasAttribute('src')) {
                     const scriptRes = await fetch(scriptElement.getAttribute('src'))
                     const scriptText = await scriptRes.text()
 
-                    const newScriptElement = document.createElement('script');
                     newScriptElement.innerHTML = scriptText
-                    document.getElementsByTagName('head')[0].appendChild(newScriptElement)
-                }else{
-                    const newScriptElement = document.createElement('script');
-                    newScriptElement.innerHTML = scriptElement.innerHTML
-                    document.getElementsByTagName('head')[0].appendChild(newScriptElement)
-                }
+                }else newScriptElement.innerHTML = scriptElement.innerHTML
+
+                document.getElementsByTagName('head')[0].appendChild(newScriptElement)
                 scriptElement.remove()
             })
-
-            HTMLComponent.setAttribute('rendered', '')
-        }else{
-            HTMLComponent.innerHTML = text.replace(propsRegex, (match, att) =>
-            HTMLComponent.getAttribute(att) || ''
-            )
-
-            HTMLComponent.querySelectorAll('script').forEach(async scriptElement => {
-                if (scriptElement.hasAttribute('src')) {
-                    const res = await fetch(scriptElement.getAttribute('src'))
-                    const data = await res.text()
-
-                    const newScriptElement = document.createElement('script');
-                    newScriptElement.innerHTML = data
-                    document.getElementsByTagName('head')[0].appendChild(newScriptElement)
-                }else{
-                    const newScriptElement = document.createElement('script');
-                    newScriptElement.innerHTML = scriptElement.innerHTML
-                    document.getElementsByTagName('head')[0].appendChild(newScriptElement)
-                }
-                scriptElement.remove()
-            })
-
-            HTMLComponent.setAttribute('rendered', '')
-
-            HTMLComponentRender(HTMLComponent)
         }
+
+        if(HTMLComponent.hasAttribute('shadow-dom')) {
+            HTMLComponent.attachShadow({mode: "open"});
+
+            HTMLComponent.shadowRoot.innerHTML = srcText.replace(propsRegex, (match, att) => HTMLComponent.getAttribute(att) || '')
+
+            loadScripts(HTMLComponent.shadowRoot.querySelectorAll('script'))
+        }else{
+            HTMLComponent.innerHTML = srcText.replace(propsRegex, (match, att) => HTMLComponent.getAttribute(att) || '')
+
+            loadScripts(HTMLComponent.querySelectorAll('script'))
+        }
+
+        HTMLComponent.setAttribute('rendered', '')
+        HTMLComponentRender(HTMLComponent)
     })
 
     container.querySelectorAll('[listener]').forEach(async listenerElement =>
         listenerElement.getAttribute('listener').split(' ').forEach(listener => {
             const [eventName, methodName] = listener.split('-')
-            if(eventName == 'load') {
-                listenerElement.addEventListener('load', event => listenerMethods[methodName](event))
-                listenerElement.dispatchEvent(new Event('load'))
-            }else{
-                listenerElement.addEventListener(eventName, event => listenerMethods[methodName](event))
-            }
+			listenerElement.addEventListener(eventName, event => HTMLComponentProps[methodName](event))
+            if(eventName == 'load') HTMLComponentProps[methodName](listenerElement.dispatchEvent(new Event('load')))
         })
     )
 }
 
-window.listenerMethods = {}
+window.HTMLComponentProps = {}
 window.HTMLComponentRender(document)
