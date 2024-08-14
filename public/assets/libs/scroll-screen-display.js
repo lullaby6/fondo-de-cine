fc261ea7d55a42f1875f52a403ea1dc7c5004953
LucianoBrumer <lucianobrumer5@gmail.com>
2024-08-14 19:59:29 -0300

    adding img title and fxing date in mobile

---
+++ public/assets/libs/scroll-screen-display.js
@@ -0,0 +1,34 @@
+
+function scrollScreenDisplay() {
+    const screenNumber = Math.floor(window.scrollY / window.innerHeight)
+
+    const scrollScreenDisplayElements = document.querySelectorAll('[scroll-screen-display]')
+
+    
+
+    scrollScreenDisplayElements.forEach((scrollScreenDisplayElement, scrollScreenDisplayElementIndex) => {
+        if (screenNumber == scrollScreenDisplayElementIndex || (screenNumber > scrollScreenDisplayElements.length && scrollScreenDisplayElementIndex == scrollScreenDisplayElements.length - 1)) {
+            scrollScreenDisplayElement.style.display = scrollScreenDisplayElement.scrollScreenDisplayOriginalDisplay
+
+            return;
+        }
+
+        scrollScreenDisplayElement.style.display = 'none'
+    })
+}
+
+function scrollScreenDisplayLoad() {
+    const scrollScreenDisplayElements = document.querySelectorAll('[scroll-screen-display]')
+
+    scrollScreenDisplayElements.forEach((element, index) => {
+        const currentDisplay = window.getComputedStyle(element).display
+        element.scrollScreenDisplayOriginalDisplay = currentDisplay == 'none' ? 'block' : currentDisplay
+        element.setAttribute('scroll-screen-display', index)
+    })
+
+    scrollScreenDisplay()
+}
+
+window.addEventListener('scroll', scrollScreenDisplay)
+
+window.addEventListener('DOMContentLoaded', scrollScreenDisplayLoad)
