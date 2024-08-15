5d92652e7a6b20a50ea3bd5c836e7c84e063bd47
LucianoBrumer <lucianobrumer5@gmail.com>
2024-08-14 21:15:11 -0300

    github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,20 @@ import alpinejs from "@astrojs/alpinejs";
 
 import icon from "astro-icon";
 
+const PORT = parseInt(import.meta.env.VITE_PORT) || 3000;
+const SITE_URL = 'https://lullaby6.github.io'
+const BASE_URL = 'fondo-de-cine'
+
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: PORT
+  },
+  site: SITE_URL,
+  base: BASE_URL,
   integrations: [
-      tailwind(), 
-      alpinejs(), 
+      tailwind(),
+      alpinejs(),
       icon({
       include: {
         iconamoon: ["arrow-down-2-light"],
